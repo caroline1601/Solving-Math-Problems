2f9ba9c3c468024f555712dbc6922c0395d6c679
caroline1601 <53101462+caroline1601@users.noreply.github.com>
2019-07-19 19:27:38 -0400

    Solving Math Problems

---
+++ UNITONESolvingMathProblemsAssignment/src/unitonesolvingmathproblemsassignment/SolvingMathProblemsAssignment.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitonesolvingmathproblemsassignment;
+
+import java.util.Scanner;
+
+public class SolvingMathProblemsAssignment {
+
+  
+    
+    public static void main(String[] args) {
+    
+        Scanner keyedinput=new Scanner(System.in);
+        
+ //Variables 
+        double length;
+        double width;
+        double area;
+
+    
+ // Commands for user 
+        System.out.println("Please enter the length of the rectangle");
+        length= keyedinput.nextDouble();
+        
+        System.out.println("Now please enter the width of the rectangle");
+        width= keyedinput.nextDouble();
+        
+        area= length * width;
+        
+        System.out.println("The area of the rectangle is " +area);
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
